74234ef29e3c55db6f03487e8bd44c8b74f751e9
oscarliusy <1069176850@qq.com>
2020-01-06 18:30:13 +0800

    L2

--- scripts/webpack.config.js
+++ scripts/webpack.config.js
@@ -1,12 +1,38 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
     entry:{
-        home:'./src/home.js',
-        about:'./src/about.js'
+        index:'./src/index.js'
     },
     output:{
         path:path.resolve(process.cwd(),"dist"),
-        filename:'[name].[chunkHash:6].js'
-    }
+        filename:'static/js/[name].[chunkHash:6].js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                  { loader: MiniCssExtractPlugin.loader },
+                  { loader: 'css-loader'  }
+                ]
+              }
+        ],
+      },
+    plugins: [
+      new HtmlWebpackPlugin({  
+        title: 'webpack Learning',
+        template: 'public/index.html'
+      }),
+      new MiniCssExtractPlugin({
+          filename:'static/css/[name].[chunkHash:6].css'
+      })
+    ],
+    devServer: {
+        port: 3000,
+        open:true
+      }
+
 }
